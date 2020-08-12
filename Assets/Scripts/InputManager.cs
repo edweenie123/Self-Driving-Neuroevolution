0092365aa0f0b857271247279b0a2e3ae58b4ceb
Edward <esong2@ocdsb.ca>
2020-08-12 15:13:55 -0400

    Map Builder Development
    Player can now place and rotate wall blocks

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -16,15 +16,18 @@ public class InputManager : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftArrow)) ChangeTime(-1f);
 
         if (Input.GetKeyUp(KeyCode.R)) SystemSettings.visualizeRayToggle = !SystemSettings.visualizeRayToggle;
+     
     }
 
+
+    
     void ChangeTime(float sign)
     {
         // update the timeScale value and ensure it's within the range [minTime, maxTime]
         timeScale += sign * changeTimeRate * Time.deltaTime;
         timeScale = Mathf.Min(maxTime, timeScale);
         timeScale = Mathf.Max(minTime, timeScale);
-
+    
         Time.timeScale = timeScale;
         // update the UI to show the correct time scale
         UIManager.EditText(UIManager.timeScaleText, "Time Scale: " + timeScale.ToString("F1") + "x");
