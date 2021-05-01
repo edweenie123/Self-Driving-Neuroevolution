0307528b75fb918dc950f768fa0f49a3ed9ff423
Edwin Chen <edwinchen1000@gmail.com>
2021-05-01 02:44:21 -0400

    added main about and track screen and naviation between them

---
+++ Assets/Scripts/MainMenuFunctions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuFunctions : MonoBehaviour
+{
+    
+    public GameObject currentScreen;
+
+    public void ShowScreen (GameObject screen)
+    {
+        currentScreen.SetActive(false);
+        screen.SetActive(true);
+        currentScreen = screen;
+    }
+}
